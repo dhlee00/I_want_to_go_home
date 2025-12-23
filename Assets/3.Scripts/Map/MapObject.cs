5810e252ee6ef530ff30502fe5f26999e518a4c6
dhlee00 <2dong0323@naver.com>
2025-12-23 16:10:17 +0900

    맵 데이터

---
+++ Assets/3.Scripts/Map/MapObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MapObject : MonoBehaviour
+{
+    // 시드
+    public string uniqueKey;
+
+    // 매쉬
+    public Transform mesh;
+
+    // 오브젝트 체력
+    public float hp;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
